a064758ada0051b7f065a37158685a7fcfd1854a
Vitor Rodrigues <vitorrodrigueshr10@gmail.com>
2020-02-06 17:17:05 -0300

    Card added

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 50.600,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 30,00 recebida de Vitor Rodrigues hoje às
+              06:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
